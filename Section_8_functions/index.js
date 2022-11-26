410925363fe6bcb3c9d024beb002e489deb4e863
Rafael Camara <rafaelcamarac@gmail.com>
2022-11-26 08:45:16 +0000

    Add functions.

---
+++ Section_8_functions/index.js
@@ -0,0 +1,13 @@
+function firstFunction(){
+    console.log("Hello from the first function.");
+}
+
+firstFunction();
+
+function numSum(x, y){
+    console.log(`The sum of ${x} and ${y} is ${x+y}`);
+    return x+y;
+}
+
+console.log(numSum(1,2));
+
